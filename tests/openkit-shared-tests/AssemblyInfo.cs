7a81a4998c1526019279c58d8a44d5bf81d7f488
Thomas Grassauer <thomas.grassauer@dynatrace.com>
2017-11-20 16:54:35 +0100

    introduced single assembly info file for tests

--- tests/openkit-shared-tests/AssemblyInfo.cs
+++ tests/openkit-shared-tests/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("openkit-dotnetfull-3.5.Tests")]
+[assembly: AssemblyTitle("OpenKit .NET Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("openkit-dotnetfull-3.5.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Dynatrace LLC")]
+[assembly: AssemblyProduct("Dynatrace.OpenKit")]
+[assembly: AssemblyCopyright("(c) 2016-2017 Dynatrace LLC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9deddc73-2ffb-406e-9cf3-5481615274f7")]
+[assembly: Guid("8b8fae28-f255-4391-8370-31dd894011c2")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
