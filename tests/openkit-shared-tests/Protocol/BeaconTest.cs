7fbc3417138875aac766daf5a6a74a3598877b38
Stefan Eberl <stefan.eberl@dynatrace.com>
2017-12-12 08:30:22 +0100

    Add some unit tests
    Trying to perform the same as in java

--- tests/openkit-shared-tests/Protocol/BeaconTest.cs
+++ tests/openkit-shared-tests/Protocol/BeaconTest.cs
@@ -30,7 +30,43 @@ namespace Dynatrace.OpenKit.Protocol
             // then
             Assert.That(target.Count, Is.EqualTo(1));
             // TODO - thomas.grassaue@dynatrace.com - ignore timestamp for now (due to static provider)
-            Assert.That(target[0], Does.StartWith(string.Format("et=60&na={0}&it=0&pa=0&s0=1&t0=", userID)));
+            Assert.That(target[0], Does.StartWith($"et=60&na={userID}&it=0&pa=0&s0=1&t0="));
+        }
+
+        [Test]
+        public void CanAddRootActionIfCaptureIsOn()
+        {
+            // given
+            var configuration = new TestConfiguration();
+            configuration.EnableCapture();
+
+            var target = new Beacon(configuration, "127.0.0.1", threadIdProvider);
+
+            // when adding the root action
+            const string RootActionName = "TestRootAction";
+            target.AddAction(new RootAction(target, RootActionName, new SynchronizedQueue<IAction>()));
+
+            // then
+            Assert.That(target.ActionDataList.Count, Is.EqualTo(1));
+            // TODO - stefan.eberl@dynatrace.com - ignore timestamp for now (due to static provider)
+            Assert.That(target.ActionDataList[0], Does.StartWith($"et=1&na={RootActionName}&it=0&ca=1&pa=0&s0=1&t0="));
+        }
+
+        [Test]
+        public void CannotAddRootActionIfCaptureIsOff()
+        {
+            // given
+            var configuration = new TestConfiguration();
+            configuration.DisableCapture();
+
+            var target = new Beacon(configuration, "127.0.0.1", threadIdProvider);
+
+            // when adding the root action
+            const string RootActionName = "TestRootAction";
+            target.AddAction(new RootAction(target, RootActionName, new SynchronizedQueue<IAction>()));
+
+            // then
+            Assert.That(target.ActionDataList, Is.Empty);
         }
 
         [Test]
