55506db1b82adf50deed1b546a90afafdcb9cc59
Johannes Baeuerle <johannes.baeuerle@dynatrace.com>
2017-12-13 16:19:58 +0100

    add unit test to increase coverage
    add tests to BeaconSendingInitStateTest and
    BeaconSendingTerminalStateTest

---
+++ tests/openkit-shared-tests/Core/Communication/BeaconSendingTerminalStateTest.cs
@@ -0,0 +1,54 @@
+﻿using Dynatrace.OpenKit.Protocol;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Dynatrace.OpenKit.Core.Communication
+{
+    [TestFixture]
+    public class BeaconSendingTerminalStateTest
+    {
+        private IHTTPClient httpClient;
+        private IBeaconSendingContext context;
+
+        [SetUp]
+        public void Setup()
+        {
+            httpClient = Substitute.For<IHTTPClient>();
+            context = Substitute.For<IBeaconSendingContext>();
+            context.GetHTTPClient().Returns(httpClient);
+        }
+
+        [Test]
+        public void StateIsTerminal()
+        {
+            // when
+            var target = new BeaconSendingTerminalState();
+
+            // then
+            Assert.That(target.IsTerminalState, Is.True);
+        }
+
+        [Test]
+        public void TerminalStateIsNextStateIfTransitionFromTerminalStateOccurs()
+        {
+            //when
+            var target = new BeaconSendingTerminalState();
+
+            //then
+            Assert.That(Is.ReferenceEquals(target.ShutdownState, target), Is.True);
+        }
+
+        [Test]
+        public void TerminalStateCallsShutdownOnExecution()
+        {
+            // given
+            
+            // when
+            var target = new BeaconSendingTerminalState();
+            target.Execute(context);
+
+            // then
+            context.Received(1).RequestShutdown();
+        }
+    }
+}
