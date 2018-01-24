915e75726042c9eb8d6596fa934d8ef31f2124c0
Stefan Eberl <stefan.eberl@dynatrace.com>
2018-01-24 17:29:00 +0100

    Add impl for BeaconCache
    Still needs implementation

---
+++ src/openkit-shared/Core/Caching/BeaconCache.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+Busing System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dynatrace.OpenKit.Core.Caching
+{
+    class BeaconCache : IBeaconCache
+    {
+        public BeaconCache()
+        {
+
+        }
+
+        public HashSet<int> BeaconIDs => throw new NotImplementedException();
+
+        public long NumBytesInCache => throw new NotImplementedException();
+
+        public event EventHandler RecordAdded;
+
+        public void AddActionData(int beaconID, long timestamp, string data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddEventData(int beaconID, long timestamp, string data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCacheEntry(int beaconID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int EvictRecordsByAge(int beaconID, long minTimestamp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int EvictRecordsByNumber(int beaconID, int numRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetNextBeaconChunk(int beaconID, string chunkPrefix, int maxSize, char delimiter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEmpty(int beaconID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveChunkedData(int beaconID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResetChunkedData(int beaconID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
