995b988a4b21fe35244711b106186989136da3bf
Stefan Eberl <stefan.eberl@dynatrace.com>
2018-02-14 11:26:09 +0100

    No further actions on stopped WebRequestTracer
    It's no longer possible to Start or set any values on already stopped
    WebRequestTracer.

---
+++ tests/openkit-shared-tests/Core/WebRequestTracerBaseTest.cs
@@ -0,0 +1,235 @@
+﻿using Dynatrace.OpenKit.API;
+using Dynatrace.OpenKit.Core.Caching;
+using Dynatrace.OpenKit.Core.Configuration;
+using Dynatrace.OpenKit.Protocol;
+using Dynatrace.OpenKit.Providers;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Dynatrace.OpenKit.Core
+{
+    public class WebRequestTracerBaseTest
+    {
+        private Beacon beacon;
+        private Action action;
+        private OpenKitConfiguration testConfiguration;
+        private ITimingProvider mockTimingProvider;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mockTimingProvider = Substitute.For<ITimingProvider>();
+            testConfiguration = new TestConfiguration();
+            beacon = new Beacon(Substitute.For<ILogger>(),
+                                new BeaconCache(),
+                                testConfiguration,
+                                "127.0.0.1",
+                                Substitute.For<IThreadIDProvider>(),
+                                mockTimingProvider);
+            action = new RootAction(beacon, "ActionName", new SynchronizedQueue<IAction>());
+        }
+
+        [Test]
+        public void DefaultValues()
+        {
+            // given
+            var target = new TestWebRequestTracerBase(beacon, action);
+
+            // then
+            Assert.That(target.URL, Is.EqualTo("<unknown>"));
+            Assert.That(target.ResponseCode, Is.EqualTo(-1));
+            Assert.That(target.StartTime, Is.EqualTo(-1L));
+            Assert.That(target.EndTime, Is.EqualTo(-1L));
+            Assert.That(target.StartSequenceNo, Is.EqualTo(2));
+            Assert.That(target.EndSequenceNo, Is.EqualTo(-1));
+            Assert.That(target.BytesSent, Is.EqualTo(-1));
+            Assert.That(target.BytesReceived, Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void GetTag()
+        {
+            // given
+            var target = new TestWebRequestTracerBase(beacon, action);
+
+            // then
+            var expectedTag = beacon.CreateTag(action, 2);
+            Assert.That(target.Tag, Is.EqualTo(expectedTag));
+        }
+
+        [Test]
+        public void ANewlyCreatedWebRequestTracerIsNotStopped()
+        {
+            // given
+            var target = new TestWebRequestTracerBase(beacon, action);
+
+            // then
+            Assert.That(target.IsStopped, Is.False);
+        }
+
+        [Test]
+        public void AWebRequestTracerIsStoppedAfterStopHasBeenCalled()
+        {
+            // given
+            var target = new TestWebRequestTracerBase(beacon, action);
+
+            // when calling the stop method
+            target.Stop();
+
+            // then
+            Assert.That(target.IsStopped, Is.True);
+        }
+
+        [Test]
+        public void SetResponseCodeSetsTheResponseCode()
+        {
+            // given
+            var target = new TestWebRequestTracerBase(beacon, action);
+
+            // when setting response code
+            var obtained = target.SetResponseCode(418);
+
+            // then
+            Assert.That(target.ResponseCode, Is.EqualTo(418));
+            Assert.That(obtained, Is.SameAs(target));
+        }
+
+        [Test]
+        public void SetResponseCodeDoesNotSetTheResponseCodeIfStopped()
+        {
+            // given
+            var target = new TestWebRequestTracerBase(beacon, action);
+            target.Stop();
+
+            // when setting response code
+            var obtained = target.SetResponseCode(418);
+
+            // then
+            Assert.That(target.ResponseCode, Is.EqualTo(-1));
+            Assert.That(obtained, Is.SameAs(target));
+        }
+
+
+        [Test]
+        public void SetBytesSentSetsTheNumberOfSentBytes()
+        {
+            // given
+            var target = new TestWebRequestTracerBase(beacon, action);
+
+            // when setting the sent bytes
+            var obtained = target.SetBytesSent(1234);
+
+            // then
+            Assert.That(target.BytesSent, Is.EqualTo(1234));
+            Assert.That(obtained, Is.SameAs(target));
+        }
+
+        [Test]
+        public void SetBytesSentDoesNotSetAnythingIfStopped()
+        {
+            // given
+            var target = new TestWebRequestTracerBase(beacon, action);
+            target.Stop();
+
+            // when setting the sent bytes
+            var obtained = target.SetBytesSent(1234);
+
+            // then
+            Assert.That(target.BytesSent, Is.EqualTo(-1));
+            Assert.That(obtained, Is.SameAs(target));
+        }
+
+        [Test]
+        public void SetBytesReceivedSetsTheNumberOfReceivedBytes()
+        {
+            // given
+            var target = new TestWebRequestTracerBase(beacon, action);
+
+            // when setting the received bytes
+            var obtained = target.SetBytesReceived(4321);
+
+            // then
+            Assert.That(target.BytesReceived, Is.EqualTo(4321));
+            Assert.That(obtained, Is.SameAs(target));
+        }
+
+        [Test]
+        public void SetBytesReceivedDoesNotSetAnythingIfStopped()
+        {
+            // given
+            var target = new TestWebRequestTracerBase(beacon, action);
+            target.Stop();
+
+            // when setting the received bytes
+            var obtained = target.SetBytesReceived(4321);
+
+            // then
+            Assert.That(target.BytesReceived, Is.EqualTo(-1));
+            Assert.That(obtained, Is.SameAs(target));
+        }
+
+        [Test]
+        public void StartSetsTheStartTime()
+        {
+            // given
+            var target = new TestWebRequestTracerBase(beacon, action);
+            mockTimingProvider.ProvideTimestampInMilliseconds().Returns(123456789L);
+
+            // when starting web request tracing
+            var obtained = target.Start();
+
+            // then
+            Assert.That(target.StartTime, Is.EqualTo(123456789L));
+            Assert.That(obtained, Is.SameAs(target));
+        }
+
+        [Test]
+        public void StartDoesNothingIfAlreadyStopped()
+        {
+            // given
+            var target = new TestWebRequestTracerBase(beacon, action);
+            mockTimingProvider.ProvideTimestampInMilliseconds().Returns(123456789L);
+            target.Stop();
+
+            // when starting web request tracing
+            var obtained = target.Start();
+
+            // then
+            Assert.That(target.StartTime, Is.EqualTo(-1L));
+            Assert.That(obtained, Is.SameAs(target));
+        }
+
+        [Test]
+        public void StopCanOnlyBeExecutedOnce()
+        {
+            // given
+            var target = new TestWebRequestTracerBase(beacon, action);
+            mockTimingProvider.ProvideTimestampInMilliseconds().Returns(123L, 321L);
+
+            // when executed the first time
+            beacon.ClearData();
+            target.Stop();
+
+            // then
+            Assert.That(target.EndSequenceNo, Is.EqualTo(3));
+            Assert.That(target.EndTime, Is.EqualTo(123L));
+            Assert.That(beacon.IsEmpty, Is.False);
+
+            // and when executed the second time
+            beacon.ClearData();
+            target.Stop();
+
+            // then
+            Assert.That(target.EndSequenceNo, Is.EqualTo(3));
+            Assert.That(target.EndTime, Is.EqualTo(123L));
+            Assert.That(beacon.IsEmpty, Is.True);
+        }
+
+        private sealed class TestWebRequestTracerBase : WebRequestTracerBase
+        {
+            public TestWebRequestTracerBase(Beacon beacon, Action action) : base(beacon, action)
+            {
+            }
+        }
+    }
+}
