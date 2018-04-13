061376f6163869481876fde5b7b6052afccb5bc8
Johannes Baeuerle <johannes.baeuerle@dynatrace.com>
2018-04-13 14:05:55 +0200

    .net style comment for thread id function

--- src/openkit-shared/Providers/DefaultThreadIDProvider.cs
+++ src/openkit-shared/Providers/DefaultThreadIDProvider.cs
@@ -23,13 +23,15 @@ namespace Dynatrace.OpenKit.Providers
     /// </summary>
     public class DefaultThreadIDProvider : IThreadIDProvider
     {
-        /*
-        * Thread.CurrentThread.ManagedThreadId returns a int value which may be negative.
-        * The Beacon protocol requires the thread id to be a positive integer value.
-        * The most significant bit is forced to '0' by a bitwise-and operation with an integer
-        * where all bits except for the most significant bit are set to '1'.
-        */
-
+        /// <summary> 
+        /// Get ID of calling thread
+        /// </summary>
+        /// <remarks>
+        /// Thread.CurrentThread.ManagedThreadId returns a int value which may be negative.
+        /// The Beacon protocol requires the thread id to be a positive integer value.
+        /// The most significant bit is forced to '0' by a bitwise-and operation with an integer
+        /// where all bits except for the most significant bit are set to '1'.
+        /// </remarks>
         public int ThreadID => Thread.CurrentThread.ManagedThreadId & 0x7fffffff;
     }
 }
