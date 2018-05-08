c75bc1b0cbd00caf7709190303d3ea305c92e33e
Stefan Eberl <stefan.eberl@dynatrace.com>
2018-05-08 15:49:12 +0200

    Add new .NET 4.7 project
    Also update all possible outdated packages to the latest version.

--- src/openkit-shared/AssemblyInfo.cs
+++ src/openkit-shared/AssemblyInfo.cs
@@ -55,5 +55,6 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("openkit-dotnetfull-4.0.Tests")]
 [assembly: InternalsVisibleTo("openkit-dotnetfull-4.5.Tests")]
 [assembly: InternalsVisibleTo("openkit-dotnetfull-4.6.Tests")]
+[assembly: InternalsVisibleTo("openkit-dotnetfull-4.7.Tests")]
 // Expose internal classes to NSubstitute
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
