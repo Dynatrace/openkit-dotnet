c190523370f4d901a453f2e41286ac6616c33eb4
Stefan Eberl <stefan.eberl@dynatrace.com>
2018-11-26 16:28:47 +0100

    URL Encode ApplicationID/DeviceID
    In addition, sometimes the underscore character _
    is reserved in Dynatrace/AppMon, therefore we
    need to URL-encode this character as well.

--- tests/openkit-shared-tests/Core/Configuration/OpenKitConfigurationTest.cs
+++ tests/openkit-shared-tests/Core/Configuration/OpenKitConfigurationTest.cs
@@ -139,6 +139,7 @@ namespace Dynatrace.OpenKit.Core.Configuration
         [Test]
         public void ADefaultConstructedConfigurationDisablesDataCollection()
         {
+            // given
             var target = CreateDefaultConfig();
 
             //when retrieving data collection level
@@ -151,6 +152,7 @@ namespace Dynatrace.OpenKit.Core.Configuration
         [Test]
         public void ADefaultConstructedConfigurationDisablesCrashReporting()
         {
+            // given
             var target = CreateDefaultConfig();
 
             //when retrieving data collection level
@@ -163,6 +165,7 @@ namespace Dynatrace.OpenKit.Core.Configuration
         [Test]
         public void ADefaultConstructedConfigurationUsesStrictTrustManager()
         {
+            // given
             var target = CreateDefaultConfig();
 
             //when retrieving SSL Trust manager
@@ -172,6 +175,25 @@ namespace Dynatrace.OpenKit.Core.Configuration
             Assert.That(sslTrustManager, Is.InstanceOf<SSLStrictTrustManager>());
         }
 
+        [Test]
+        public void GetApplicationID()
+        {
+            // given
+            var target = CreateDefaultConfig();
+
+            // then
+            Assert.That(target.ApplicationID, Is.EqualTo("/App_ID%"));
+        }
+
+        [Test]
+        public void GetApplicationIDPercentEncodedDoesProperEncoding()
+        {
+            // given
+            var target = CreateDefaultConfig();
+
+            // then
+            Assert.That(target.ApplicationIDPercentEncoded, Is.EqualTo("%2FApp%5FID%25"));
+        }
 
         private static OpenKitConfiguration CreateDefaultConfig()
         {
@@ -181,8 +203,8 @@ namespace Dynatrace.OpenKit.Core.Configuration
                 BeaconCacheConfiguration.DEFAULT_UPPER_MEMORY_BOUNDARY_IN_BYTES);
 
             var defaultBeaconConfig = new BeaconConfiguration();
-
-            return new OpenKitConfiguration(OpenKitType.DYNATRACE, "", "", 0, "", new Providers.TestSessionIDProvider(),
+            
+            return new OpenKitConfiguration(OpenKitType.DYNATRACE, "", "/App_ID%", 0, "", new Providers.TestSessionIDProvider(),
                   new SSLStrictTrustManager(), new Core.Device("", "", ""), "", defaultCacheConfig, defaultBeaconConfig);
         }
     }
