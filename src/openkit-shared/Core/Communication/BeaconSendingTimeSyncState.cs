82e8f72349a228d4f1b7183c0fed526ac541442b
Stefan Eberl <stefan.eberl@dynatrace.com>
2019-02-12 16:57:35 +0100

    Cluster time offset can be negative

--- src/openkit-shared/Core/Communication/BeaconSendingTimeSyncState.cs
+++ src/openkit-shared/Core/Communication/BeaconSendingTimeSyncState.cs
@@ -108,10 +108,6 @@ namespace Dynatrace.OpenKit.Core.Communication
 
             //sanity check to catch case with div/0
             var calculatedOffset = ComputeClusterTimeOffset(response.timeSyncOffsets);
-            if (calculatedOffset < 0)
-            {
-                return;
-            }
 
             // initialize time provider with cluster time offset
             context.InitializeTimeSync(calculatedOffset, true);
@@ -241,7 +237,7 @@ namespace Dynatrace.OpenKit.Core.Communication
 
             if (count == 0)
             { // shouldn't come here under normal circumstances
-                return -1; // prevents div/0
+                return 0; // prevents div/0
             }
 
             return (long)Math.Round(sum / (double)count);
