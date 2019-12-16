af3a352f3a447842300d6fa660a5bdffacb3d1e0
Stefan Eberl <stefan.eberl@dynatrace.com>
2019-12-16 17:21:35 +0100

    Merge pull request #70 in OP/openkit-dotnet from bugfix/try-fix-coverage-build to release/1.4
    * commit 'fc56ad2076ade678c1517c3fa365f9376121b95e':
      Remove unwanted files again
      Try to fix coverage build
    
    GitOrigin-RevId: e03b5614e915c3033158135c8c9cd31371f77ea1

--- callTest.ps1
+++ callTest.ps1
@@ -1,6 +1,6 @@
 param([string]$option = "Release")
 
-$workspace=(Get-Location).toString()
+$workspace = (Get-Location).toString()
 
 if ("$env:APPVEYOR" -ieq "true") {
     $format = "AppVeyor"
@@ -9,29 +9,27 @@ if ("$env:APPVEYOR" -ieq "true") {
 } else {
     $format = "nunit3"
     $outputDir = "$env:OUTPUTDIR"
-    $nunit3_console = "$workspace\packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe"
+    $nunit3_console = "$env:USERPROFILE\.nuget\packages\nunit.consolerunner\3.8.0\tools\nunit3-console.exe"
 }
 
-if($option -eq "Release") {
-    $testAssemblies = Get-ChildItem -Recurse -Include openkit-dotnetfull-*Tests.dll,openkit-dotnetstandard-*Tests.dll,openkit-dotnetpcl-*Tests.dll  | ? {$_.FullName -match "\\bin\\Release\\" } | % FullName
+if ($option -eq "Release") {
+    $testAssemblies = Get-ChildItem -Recurse -Include openkit-dotnetfull-*Tests.dll, openkit-dotnetstandard-*Tests.dll, openkit-dotnetpcl-*Tests.dll | ? { $_.FullName -match "\\bin\\Release\\" } | % FullName
     & $nunit3_console --result="$outputDir\myresults.xml;format=$format" $testAssemblies
 
     # Run .NET Core tests
-    $testProjects = Get-ChildItem -Recurse -Include openkit-dotnetcore-*Tests.csproj  | % FullName
-    foreach ($project in $testProjects)
-    {
+    $testProjects = Get-ChildItem -Recurse -Include openkit-dotnetcore-*Tests.csproj | % FullName
+    foreach ($project in $testProjects) {
         & dotnet.exe test -c Release $project --results-directory $outputDir --logger trx --no-build
     }
-} elseif($option -eq "Coverage") {
-    $openCoverConsole = "$env:USERPROFILE\.nuget\packages\opencover\4.7.922\tools\OpenCover.Console.exe"
+} elseif ($option -eq "Coverage") {
+    $openCoverConsole = "$env:USERPROFILE\.nuget\packages\opencover\4.6.519\tools\OpenCover.Console.exe"
     # Run .NET test assemblies, excluding .NET Core
-    $testAssemblies = Get-ChildItem -Recurse -Include openkit-dotnetfull-*Tests.dll,openkit-dotnetstandard-*Tests.dll,openkit-dotnetpcl-*Tests.dll  | ? {$_.FullName -match "\\bin\\Coverage\\" } | % FullName 
+    $testAssemblies = Get-ChildItem -Recurse -Include openkit-dotnetfull-*Tests.dll, openkit-dotnetstandard-*Tests.dll, openkit-dotnetpcl-*Tests.dll | ? { $_.FullName -match "\\bin\\Coverage\\" } | % FullName 
     & $openCoverConsole "-target:$nunit3_console" "-targetargs:--result=$outputDir\myresults.xml;format=$format $testAssemblies" -register:user -filter:"+[*]* -[*.Tests]*" -output:$outputDir\coverage.xml
 
     # Run .NET Core tests
-    $testProjects = Get-ChildItem -Recurse -Include openkit-dotnetcore-*Tests.csproj  | % FullName
-    foreach ($project in $testProjects)
-    {
+    $testProjects = Get-ChildItem -Recurse -Include openkit-dotnetcore-*Tests.csproj | % FullName
+    foreach ($project in $testProjects) {
         & $openCoverConsole "-target:dotnet.exe" -targetargs:"test -c Coverage $project --no-build" -register:user  -filter:"+[*]* -[*.Tests]*" -oldstyle -output:$outputDir\coverage.xml -mergeoutput
     }
 }
