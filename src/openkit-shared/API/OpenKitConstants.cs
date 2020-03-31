dfcaa94e287f14c2220ed8c06eb632195be192cb
Stefan Eberl <stefan.eberl@dynatrace.com>
2020-03-31 14:10:04 +0200

    Merge pull request #95 in OP/openkit-dotnet from pump-version-to-1.0.5 to release/1.0
    * commit 'a305ba1771ff6ef90e93b8d4191c90ded17cdd05':
      Pump version to 1.0.5
    
    GitOrigin-RevId: eb1eab7018a4574f94c4b07a4c8abc2992fc67b5

--- src/openkit-shared/API/OpenKitConstants.cs
+++ src/openkit-shared/API/OpenKitConstants.cs
@@ -22,7 +22,7 @@ namespace Dynatrace.OpenKit.API
     public class OpenKitConstants
     {
         // default values used in configuration
-        public const string DEFAULT_APPLICATION_VERSION = "1.0.4";
+        public const string DEFAULT_APPLICATION_VERSION = "1.0.5";
         public const string DEFAULT_OPERATING_SYSTEM = "OpenKit " + DEFAULT_APPLICATION_VERSION;
         public const string DEFAULT_MANUFACTURER = "Dynatrace";
         public const string DEFAULT_MODEL_ID = "OpenKitDevice";
--- src/openkit-shared/AssemblyInfo.cs
+++ src/openkit-shared/AssemblyInfo.cs
@@ -45,8 +45,8 @@ using System.Runtime.CompilerServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.4.0")]
-[assembly: AssemblyFileVersion("1.0.4.0")]
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
 
 // Expose internal classes to test assemblies
 [assembly: InternalsVisibleTo("openkit-dotnetcore-2.0.Tests, PublicKey=" + AssemblyInfoConstants.PublicKey)]
